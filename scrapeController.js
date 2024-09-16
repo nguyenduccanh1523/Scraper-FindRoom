aa75fd37f1e6e379c7ae057aced3c6f40a056bcd
nguyenduccanh1523 <nguyenduccanh2398@gmail.com>
2024-09-16 20:37:32 +0700

    create puppeter scraperCategory

---
+++ scrapeController.js
@@ -0,0 +1,28 @@
+const scrapers = require('./scraper')
+// const fs = require('fs')
+
+const scrapeController = async (browserInstance) => {
+    const url = 'https://phongtro123.com/'
+    try {
+        let browser = await browserInstance
+        // gọi hàm cạo ở file s scrape
+        let categories = await scrapers.scrapeCategory(browser, url)
+        // console.log(categories);
+        // categories.forEach(async (category) => {
+        //     const items = await scrapers.scrapeItems(browser, category.link)
+        //     console.log(items);
+        //     // items.forEach(async (item) => {
+        //     //     const rs = await scrapers.scraper(browser, item)
+        //     //     fs.writeFile('ecommerce.json', JSON.stringify({ [category]: rs }), (err) => {
+        //     //         if (err) console.log('Ghi data vô file json thất bại: ' + err)
+        //     //         console.log('Thêm data thanh công !.')
+        //     //     })
+        //     // })
+        // })
+        // await browser.close()
+    } catch (e) {
+        console.log('Lỗi ở scrape controller: ' + e);
+    }
+}
+
+module.exports = scrapeController
